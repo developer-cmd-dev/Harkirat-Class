d5377a56352e855869840a7ca11082ac9b44abd8
Dev kumar <devkmandal0@gmail.com>
2025-08-25 14:11:54 +0530

    Update index.js response format and add nodemon to package.json

--- CreateFirstServer/index.js
+++ CreateFirstServer/index.js
@@ -3,6 +3,6 @@ import express from 'express'
 
 const app = express();
 
-app.get("/",(req,res)=>res.status(200).send("Server is running"))
+app.get("/",(req,res)=>res.status(200).json({status:200,message:"Success"}))
 
 app.listen(3000,()=>console.log(`Server is running on ${3000} port`))
