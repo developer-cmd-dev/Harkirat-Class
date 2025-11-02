bf054dcc2a248d523213650668109bc37482d347
Dev kumar <devkmandal0@gmail.com>
2025-11-02 10:25:18 +0530

    Learnt to connect PG db and write custom query

--- SQLdb/src/index.ts
+++ SQLdb/src/index.ts
@@ -1,3 +1,15 @@
-console.log("This is Postgre Sql database");
+
+import express from 'express';
+
+import {Client} from 'pg';
+
+const pgClient = new Client("postgresql://neondb_owner:npg_aAi8nbojG9JS@ep-bitter-sound-ads8ov60-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 
+async  function main(){
+    await pgClient.connect();
+    const response =await pgClient.query('Select * from users where id=2');
+    console.log(response.rows);
+}
+
+main();
