1f6fd7d4018ceef465ba04fecadc8bc1f382e585
Dev kumar <devkmandal0@gmail.com>
2025-11-04 08:23:14 +0530

    Added signup and create todo endpoint

--- Prisma/dist/index.js
+++ Prisma/dist/index.js
@@ -1,23 +1,48 @@
 import { PrismaClient } from "@prisma/client";
+import express, {} from 'express';
+import * as assert from "node:assert";
 const client = new PrismaClient();
-async function main() {
-    // const response = await client.users.create({
-    //     data:{
-    //         username:"admin",
-    //         email:"admin@gmail.com",
-    //         password:"admin",
-    //         age:45,
-    //     }
-    // })
-    const find = await client.users.findFirst({
-        where: {
-            id: 1
-        },
-        select: {
-            username: true
-        }
-    });
-    console.log(find);
-}
-main();
+const app = express();
+const port = 4000;
+client.$connect().then(() => {
+    console.log("Connected to Prisma client...");
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+}).catch(err => console.log(err));
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+    const { username, email, password, age } = req.body;
+    try {
+        const response = await client.users.create({
+            data: {
+                username: username,
+                email: email,
+                password: password,
+                age: age,
+            }
+        });
+        res.status(200).json(response);
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error });
+    }
+});
+app.post("/createTodo/:id", async (req, res) => {
+    const { title, discription, completed } = req.body;
+    const userId = req.params.id;
+    try {
+        const todoResponse = await client.todo.create({
+            data: {
+                title: title,
+                discription: discription,
+                completed: completed,
+                userId: Number(userId),
+            }
+        });
+        res.status(200).json(todoResponse);
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
 //# sourceMappingURL=index.js.map
--- Prisma/src/index.ts
+++ Prisma/src/index.ts
@@ -1,31 +1,73 @@
 import {PrismaClient} from "@prisma/client"
+import express,{type Response,type Request}from 'express';
+import * as assert from "node:assert";
+
 
 
 const client = new PrismaClient();
+const app = express();
+const port = 4000;
 
-async function main(){
- // const response = await client.users.create({
- //     data:{
- //         username:"admin",
- //         email:"admin@gmail.com",
- //         password:"admin",
- //         age:45,
- //     }
- // })
+client.$connect().then(()=>{
+    console.log("Connected to Prisma client...");
+    app.listen(port,()=>console.log(`Listening on port ${port}`));
 
 
-    const find = await client.users.findFirst({
-        where:{
-            id:1
-        },
-        select:{
-            username:true
+}).catch(err=>console.log(err));
+
+
+app.use(express.json());
+
+app.post("/signup",async (req:Request,res:Response)=>{
+const {username,email,password,age} = req.body;
+
+try {
+    const response = await client.users.create({
+        data:{
+            username:username,
+            email:email,
+            password:password,
+            age:age,
         }
     })
 
-
-
-    console.log(find);
+    res.status(200).json(response);
+}catch (error){
+    console.log(error)
+    res.status(500).json({error:error})
 }
 
-main();
+
+})
+
+
+
+
+app.post("/createTodo/:id",async (req:Request,res:Response)=>{
+    const {title,discription,completed}=req.body;
+    const userId = req.params.id;
+
+
+  try{
+
+
+         const todoResponse = await client.todo.create({
+              data:{
+                  title:title,
+                  discription:discription,
+                  completed:completed,
+                  userId:Number(userId),
+              }
+          })
+
+          res.status(200).json(todoResponse)
+
+
+
+
+  }catch (error){
+      console.log(error)
+  }
+
+})
+
