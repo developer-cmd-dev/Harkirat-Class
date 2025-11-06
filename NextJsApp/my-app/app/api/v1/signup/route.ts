bffefa71b11e82a46ae44e5dd19bf2f92a5ba8a0
Dev kumar <devkmandal0@gmail.com>
2025-11-06 08:52:37 +0530

    Add Prisma setup and user model with signup functionality

--- NextJsApp/my-app/app/api/v1/signup/route.ts
+++ NextJsApp/my-app/app/api/v1/signup/route.ts
@@ -1,14 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
 
 type UserData = {
     password:String,
     email:String
 }
 
+const client = new PrismaClient();
+
 export async function POST(req:NextRequest,res:NextResponse) {
-    const {email,password}:UserData = await req.json();
-    console.log(email,password)
-    return NextResponse.json({
-        message:"success"
+    const {email,password} = await req.json();
+   try{
+     const response = await client.user.create({
+        data:{
+            email:email,
+            password:password,
+        }
     })
+    return NextResponse.json({
+        message:"success",
+        data:response
+    })
+   }catch(error){
+    console.log(error)
+    return;
+   }
 }
