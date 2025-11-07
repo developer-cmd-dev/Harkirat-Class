c7943ba97c8ec68fe9563e1f9636a919030bcab8
Dev kumar <devkmandal0@gmail.com>
2025-11-07 06:45:27 +0530

    feat: initialize Next.js application with TypeScript and Tailwind CSS
    - Added package.json with dependencies for React, Next.js, TypeScript, and Tailwind CSS.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Added SVG assets: file.svg, globe.svg, next.svg, vercel.svg, window.svg.
    - Configured TypeScript with tsconfig.json for strict type checking and Next.js support.

---
+++ SecondNextJs/my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function Home() {
+  return (
+   <div className=" h-screen flex items-center justify-center ">
+      <div className=" ">
+          <Link className="bg-green-500 py-2 px-5 rounded-xl text-2xl text-black hover:bg-white" href={"/signup"}>Sign Up</Link>
+      </div>
+      
+    </div>
+  );
+}
